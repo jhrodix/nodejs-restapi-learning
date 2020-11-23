a27abbee288c9ebdc76d03bc9ec675600c67a812
gabrielRodas <galejandrorodas@gmail.com>
2020-11-23 00:24:58 -0600

    Agregando gestion de equipos

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const {Client}=require('pg');
 client = new Client({
     host: 'localhost',
     user: 'postgres',
-    password: 'xxxxx',
+    password: 'postgres',
     database: 'usuarios',
 });
 
@@ -41,7 +41,7 @@ client.connect(
     });
 
 /* Respondiendo a solicitud get en la ruta /usuario */
-app.get('/usuario/:usuario', function (req, _res) {
+app.get('/usuario/:usuario', (req, _res) => {
     
     client.query('SELECT nombre,apellido from usuarios where nombre=$1::text', [req.params.usuario], (err, res) => 
     {	
@@ -130,6 +130,270 @@ app.delete('/usuario', function (req, _res) {
 		}
 		_res.send(respuesta);
 	});
+});
+
+//Metodo GET para los equipos
+app.get('/equipos/:equipo', function(req, _res){
+	client.query('SELECT * from equipos where nombre=$1::text', [req.params.equipo], (err, res) =>
+	{
+		//si el equipo existe
+		if(res.rows.length>0)
+		{
+			console.log('GET buscando equipo');
+			respuesta = {
+				error: false,
+				codigo: 200,
+				mensaje: 'Todo salio bien',
+				equipo: {
+					nombre: res.rows[0].nombre,
+					pais: res.rows[0].pais,
+					creacion: res.rows[0].creacion,
+					logo: res.rows[0].logo,
+					ganados: res.rows[0].ganados
+				}
+			};
+		}
+		else
+		{
+			console.log('GET equipo no encontrado');
+			respuesta = {
+				error: true,
+				codigo: 404,
+				mensaje: 'equipo no encontrado',
+			};
+
+		}
+		_res.send(respuesta);
+	});
+
+});
+
+//Metodo POST para los equipos
+app.post('/equipos', function(req, _res){
+	client.query('SELECT * from equipos where nombre=$1::text', [req.body.nombre], (err, res_select) =>
+	{
+		if(res_select.rows.length==0){
+			client.query('INSERT into equipos values($1::text,$2::text,$3::date,$4::text,$5::text)', [req.body.nombre,req.body.pais,req.body.creacion,req.body.logo,req.body.ganados], (errorDB, res) =>
+			{
+				console.log("POST creando un nuevo equipo");
+				console.log(errorDB);
+				//codigo para validar que el equipo se inserto bien
+				let respuesta =
+				{
+					error: false,
+					codigo: 200,
+					mensaje: 'Equipo Creado',
+					equipo: {
+						nombre: req.body.nombre,
+						pais: req.body.pais,
+						creacion: req.body.creacion,						
+						logo: req.body.logo,	
+						ganados: req.body.ganados					
+					}
+				}
+				_res.send(respuesta);
+			});
+		}
+		else
+		{
+			console.log("Equipo ya existe");
+			let respuesta =
+			{
+				error: true,
+				codigo: 404,
+				mensaje: 'Equipo no creado',
+				equipo: {
+					nombre: req.body.nombre,
+					pais: req.body.pais,
+					creacion: req.body.creacion,
+					logo: req.body.logo,	
+					ganados: req.body.ganados									
+				}
+			}
+			_res.send(respuesta);
+		}				
+	});
+
+});
+
+//Metodo PUT para actualizar la tabla equipos
+app.put("/equipos/:nombre", function(req, _res){
+	client.query('UPDATE equipos set nombre=$1::text, pais=$2::text, creacion=$3::date, logo=$4::text, ganados=$5::text WHERE nombre=$6::text',	[req.body.nombre,req.body.pais,req.body.creacion,req.body.logo,req.body.ganados,req.params.nombre], (errorDB, res) =>
+	{
+		console.log('PUT actualizando equipo');
+		console.log(errorDB);
+		let respuesta =
+		{
+			error: false,
+			codigo: 200,
+			mensaje: 'equipo modificado',
+			equipo: {
+				nombre: req.body.nombre,
+				pais: req.body.pais,
+				creacion: req.body.creacion,
+				logo: req.body.logo,
+				ganados: req.body.ganados
+			}
+		}
+		_res.send(respuesta);
+	});
+
+});
+
+//Metodo DELETE para eliminar equipos
+app.delete('/equipos', function(req, _res){
+	client.query('DELETE from equipos where nombre=$1::text', [req.body.nombre], (errorDB, res) => 
+	{
+		console.log('DELETE eliminando equipo');
+		console.log(errorDB);
+		let respuesta =
+		{
+			error: false,
+			codigo: 200,
+			mensaje: 'equipo eliminado',
+			equipo: {
+				nombre: req.body.nombre
+			}
+		}
+		_res.send(respuesta);
+	});
+
+});
+
+//Metodo GET para los jugadores
+app.get('/jugadores/:nombre/:apellido', function(req, _res){
+	client.query('SELECT * FROM jugadores WHERE nombre=$1::text AND apellido=$2::text', [req.params.nombre,req.params.apellido], (errorDB, res) =>
+	{
+		//si el jugador existe
+		if(res.rows.length>0)
+		{
+			console.log('GET buscando jugador');
+			console.log(errorDB);
+
+			respuesta =
+			{
+				error: false,
+				codigo: 200,
+				mensaje: 'todo salio bien',
+				jugador: {
+					nombre: res.rows[0].nombre,
+					apellido: res.rows[0].apellido,
+					nacimiento: res.rows[0].nacimiento,
+					nacionalidad: res.rows[0].nacionalidad,
+					equipo: "/equipo/"+res.rows[0].equipo
+				}
+			}
+		}
+		else
+		{
+			console.log('GET jugador no encontrado');
+			respuesta = {
+				error: true,
+				codigo: 404,
+				mensaje: 'El jugador no existe'
+			}
+
+		}
+		_res.send(respuesta);
+	}); 
+	
+});
+
+//Metodo POST para agregar jugadores
+app.post('/jugadores', function(req, _res) {
+	client.query('SELECT * FROM jugadores WHERE nombre=$1::text AND apellido=$2::text', [req.body.nombre,req.body.apellido], (err, res_select) =>
+	{
+		if(res_select.rows.length==0){
+			client.query('INSERT INTO jugadores VALUES($1::text,$2::text,$3::date,$4::text,$5::text)', [req.body.nombre,req.body.apellido,req.body.nacimiento,req.body.nacionalidad,req.body.equipo], (errorDB,res) =>
+			{
+				console.log('POST creando un nuevo jugador');
+				console.log(errorDB);
+				//codigo para validar si todo esta bien
+				let respuesta = 
+				{
+					error: false,
+					codigo: 200,
+					mensaje: 'jugador agregado',
+					jugador: {
+						nombre: req.body.nombre,
+						apellido: req.body.apellido,
+						nacimiento: req.body.nacimiento,
+						nacionalidad: req.body.nacionalidad,
+						equipo: req.body.equipo
+					}
+				}
+				_res.send(respuesta);
+			});
+		}
+		else
+		{
+			console.log('jugador ya existe');
+			let respuesta =
+			{
+				error: true,
+				codigo: 404,
+				mensaje: 'jugador no creado',
+				jugador: {
+					nombre: req.body.nombre,
+					apellido: req.body.apellido,
+					nacimiento: req.body.nacimiento,
+					nacionalidad: req.body.nacionalidad,
+					equipo: req.body.equipo
+				}
+			}
+			_res.send(respuesta);
+		}
+		
+	});
+
+});
+
+//Metodo PUT para actualizar jugadores
+app.put('/jugadores/:nombre/:apellido', function(req, _res){
+	client.query('UPDATE jugadores set nombre=$1::text,apellido=$2::text,nacimiento=$3::date,nacionalidad=$4::text,equipo=$5::text WHERE nombre=$6 AND apellido=$7::text', [req.body.nombre,req.body.apellido,req.body.nacimiento,req.body.nacionalidad,req.body.equipo,req.params.nombre,req.params.apellido], (err, res) =>
+	{
+		console.log("PUT actualizando jugador")
+		console.log(err)
+		let respuesta =
+		{
+			error: false,
+			codigo: 200,
+			mensaje: 'jugador modificado',
+			jugador: {
+				nombre: req.body.nombre,
+				apellido: req.body.apellido,
+				nacimiento: req.body.nacimiento,
+				nacionalidad: req.body.nacionalidad,
+				equipo: req.body.equipo
+			}
+		}
+		_res.send(respuesta);
+	});
+});
+
+//Metodo para eliminar jugadores
+app.delete('/jugadores', function(req, _res){
+	client.query('DELETE FROM jugadores WHERE nombre=$1::text AND apellido=$2::text', [req.body.nombre,req.body.apellido], (err, res) =>
+	{
+		console.log('DELETE eliminando jugador');
+		console.log(err);
+
+		let respuesta =
+		{
+			error: false,
+			codigo: 200,
+			mensaje: 'jugador eliminado',
+			jugador: {
+				nombre: req.body.nombre,
+				apellido: req.body.apellido,
+				nacimiento: req.body.nacimiento,
+				nacionalidad: req.body.nacionalidad,
+				equipo: req.body.equipo
+			}
+		}
+		_res.send(respuesta);
+	});
+
 });
 
 /*mostrar error en caso de no existir la url*/
